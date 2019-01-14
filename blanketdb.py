64f83e510c52f2f976e02b8eef92f08a1c0d95c1
luphord <luphord@protonmail.com>
2019-01-14 07:05:06 +0100

    function for parsing and type converting forms

--- blanketdb.py
+++ blanketdb.py
@@ -1,7 +1,29 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Top-level package for BlanketDB."""
+'''A simple HTTP accessible database for IoT projects.'''
 
-__author__ = """luphord"""
+__author__ = 'luphord'
 __email__ = 'luphord@protonmail.com'
 __version__ = '0.1.0'
+
+import os, json, sqlite3
+from datetime import datetime, date, timedelta
+import urllib.parse
+
+def parse_form(form_s):
+    d = dict()
+    for k, v in urllib.parse.parse_qsl(form_s):
+        try:
+            d[k] = int(v)
+        except ValueError:
+            try:
+                d[k] = float(v)
+            except ValueError:
+                if v.lower() == 'true':
+                    d[k] = True
+                elif v.lower() == 'false':
+                    d[k] = False
+                else:
+                    d[k] = v
+    return d
--- tests/test_blanketdb.py
+++ tests/test_blanketdb.py
@@ -6,7 +6,7 @@
 
 import unittest
 
-import blanketdb
+from blanketdb import parse_form
 
 
 class TestBlanketdb(unittest.TestCase):
@@ -20,3 +20,13 @@ class TestBlanketdb(unittest.TestCase):
 
     def test_000_something(self):
         """Test something."""
+
+    def test_parse_form(self):
+        '''Test function for parsing and type converting forms'''
+        self.assertEqual(dict(a='b'), parse_form('a=b'))
+        self.assertEqual(dict(a='b',c='d'), parse_form('a=b&c=d'))
+        self.assertEqual(dict(a=1), parse_form('a=1'))
+        self.assertEqual(1.1, parse_form('a=1.1')['a'])
+        self.assertEqual(float, type(parse_form('a=1.0')['a']))
+        self.assertEqual(dict(a=True), parse_form('a=true'))
+        self.assertEqual(dict(a=False), parse_form('a=false'))
